209f550641505feb05d49da86588ec9374734b47
Matthew Kotys <mattkotys@gmail.com>
2017-11-04 11:19:53 -0500

    added testing capabilities to main.handlebars

--- meadowlark.js
+++ meadowlark.js
@@ -1,16 +1,21 @@
-var express = require('express');
+var express = require('express')
 
-var app = express();
+var app = express()
 
 // set up handlebars view engine
 var handlebars = require('express-handlebars')
-	.create({ defaultLayout:'main' });
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
+	.create({ defaultLayout:'main' })
+app.engine('handlebars', handlebars.engine)
+app.set('view engine', 'handlebars')
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000)
 
-app.use(express.static(__dirname + '/public'));
+app.use(function(req, res, next) {
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test === 1
+	next()
+})
+
+app.use(express.static(__dirname + '/public'))
 
 var fortuneCookies = [
 	"Conquer your fears or they will conquer you.",
@@ -18,29 +23,29 @@ var fortuneCookies = [
 	"Do not fear what you don't know.",
 	"You will have a pleasant surprise.",
 	"Whenever possible, keep it simple.",
-];
+]
 
 app.get('/', function(req, res) {
 	res.render('home');
-});
+})
 app.get('/about', function(req,res){
 	var randomFortune =
 		fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
 	res.render('about', { fortune: randomFortune });
-});
+})
 
 // 404 catch-all handler (middleware)
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
 	res.status(404);
 	res.render('404');
-});
+})
 
 // 500 error handler (middleware)
-app.use(function(err, req, res, next){
+app.use(function(err, req, res, next) {
 	console.error(err.stack);
 	res.status(500);
 	res.render('500');
-});
+})
 
 app.listen(app.get('port'), function(){
   console.log( 'Express started on http://localhost:' +
